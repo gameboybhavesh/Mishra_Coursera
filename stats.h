5d88caa09da286196678a7cb6558770e52e9f1a7
Bhavesh <bhaveshmishra567@gmail.com>
2023-11-23 15:12:48 +0530

    latest

--- stats.h
+++ stats.h
@@ -24,26 +24,95 @@
 #define __STATS_H__
 
 /* Add Your Declarations and Function Comments here */ 
+
+/** '#######################################################
+ * @brief Find minimum value
+ *
+ * A function which finds the minimum value of a given array.
+ * 
+ *
+ * @param unsigned char,int
+ *
+ *@return minimum value
+ ###########################################################*/ 
 unsigned char find_minimum(unsigned char test[], int size);
+
+ /** '########################################################
+ * @brief Find maximum value
+ *
+ * A function which finds the maximum value of a given array.
+ * 
+ *
+ * @param unsigned char,int
+ *
+ *@return maximum value
+ ###########################################################*/ 
 unsigned char find_maximum(unsigned char test[], int size);
+
+/** '########################################################
+ * @brief Find mean value
+ *
+ * A function which finds the average (mean) value of a given array.
+ * 
+ *
+ * @param unsigned char,int
+ *
+ *@return mean value
+ ###########################################################*/ 
 unsigned char find_mean(unsigned char test[], int size);
+
+/** '#########################################################
+ * @brief Find median value
+ *
+ * A function which finds the median value of the given array.
+ * 
+ *
+ * @param unsigned char,int
+ *
+ *@return median value
+ ###########################################################*/ 
+
 unsigned char find_median(unsigned char test[], int size);
+
+
+ /** '########################################################
+ * @brief Reorganize given values
+ *
+ * A function which sorts / reorganizes given numbers from largest to smallest
+ * 
+ *
+ * @param unsigned char,int
+ *
+ * @return sorted_array
+ ###########################################################*/ 
 unsigned char sort_array(unsigned char test[], int size);
+/**#########################################################
+ * @brief Prints statistic values
+ *
+ * A function that prints the statistics of an array including minimum, 
+ * maximum, mean and median in a formatted way.
+ *
+ * @param unsigned char min <minimum value>
+ * @param unsigned char max <maximum value>
+ * @param unsigned char mean  <average value>
+ * @param unsigned char median <median value>
+ *
+ * @return void <no return value>
+ ##########################################################*/
 void print_statistics(unsigned char test[], int size);
+
+ /** '########################################################
+ * @brief Print an array 
+ *
+ * A function which prints a given array in a readable formatted way
+ * 
+ *
+ * @param unsigned char,int 
+ * 
+ *@return void <no return value>
+ ###########################################################*/ 
 void print_array(unsigned char test[], int size);
-/**
- * @brief <Add Brief Description of Function Here>
- *
- * <Add Extended Description Here>
- *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- *
- * @return <Add Return Informaiton here>
- */
- 
+
 
 #endif /* __STATS_H__ */
 
